91d18550c716a0832045a2f60231aa8cb0e61b5f
Janna Ashley <quiban.241127@sjdelmonte.sti.edu.ph>
2025-01-27 22:26:55 +0800

    With comments
    I added comments to give a better understanding of my program.

--- TA1/program3.py
+++ TA1/program3.py
@@ -1,21 +1,23 @@
+# Prints a right-aligned triangle using loops
 print("\n---- Part a: Right-aligned triangle")
-n = 5 
+n = 5 # number of rows
 for row in range(1, n + 1):
-    for space in range(n - row):
+    for space in range(n - row): # print spaces
         print(" ", end="")
-    for num in range(1, row + 1):
+    for num in range(1, row + 1): # print numbers
         print(num, end="")
     print()  # move to the next line
 
+# Prints a pattern using loops
 print("\n---- Part b: Pattern")
-skip = 1  
-row = 1  
+skip = 1  # number of digits to skip
+row = 1  # number of rows
 while row <= 5: 
-    count = 1
-    while count <= skip:
+    count = 1 # initialize the counter
+    while count <= skip: # loop to print the numbers
         print(skip, end="")
         count += 1
-    print() # move to the next line
-    skip += 2
-    row += 1 
+    print() 
+    skip += 2 # increment the number of digits to skip
+    row += 1 # increment the number of rows
 print()
