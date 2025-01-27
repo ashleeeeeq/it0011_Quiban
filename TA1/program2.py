5d0133cfe21edc3eb4d1c8b7fae1cee63c9a6d4e
Janna Ashley <quiban.241127@sjdelmonte.sti.edu.ph>
2025-01-27 22:07:05 +0800

    With comments
    Added comments for a better understanding of the program.

--- TA1/program2.py
+++ TA1/program2.py
@@ -1,11 +1,13 @@
+# Getting user input for a string containing digits
 input_string = input("\n---- Please enter a string containing digits: ")
 
 str_digit = 0
 
 for char in input_string:
-    if char.isdigit():  
-        str_digit += int(char) 
+    if char.isdigit():  # check if the character is a digit
+        str_digit += int(char) # add the digit to the sum
 
+# Output the sum of digits in the string
 print("\n┌──\t\t  RESULTS\t\t  ──┐\n")
 print("\tSum of digits in the string:", str_digit)
 print("\n└──\t\t\t\t\t  ──┘\n")
